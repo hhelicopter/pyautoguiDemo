8a6298343c611556de3b32f7375e54c6d938d848
hhelicopter <34024905+hhelicopter@users.noreply.github.com>
2021-11-23 23:36:11 +0800

    add

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import pyautogui
 
+pyautogui.FAILSAFE = True # 保护措施，鼠标光标在屏幕左上角会产生FailSafeException异常。
+pyautogui.PAUSE = 2.5 #函数增加延迟为2.5秒
+
 def get_mouse_position():
     '''
     获取鼠标当前位置
@@ -41,12 +44,39 @@ def move_mouse_rel(x,y,duration=0.2):
     :param x:横向偏移量
     :param y:横向偏移量
     :param duration:延时
-    :return:
+    :return:None
     '''
     pyautogui.moveRel(x, y, duration=0.2)
 
-def mouse_click():
-    pass
+def move_mouse_with_drag(x, y, time=2, button='left'):
+    '''
+    按下鼠标键同时移动到屏幕指定位置，
+    :param x: 横坐标
+    :param y: 纵坐标
+    :param time: 点击时间
+    :param button: 鼠标键位
+    :return:None
+    '''
+    pyautogui.dragTo(x, y, time=time, button=button)
+
+def mouse_click(clicks,button='left',interval=0.25):
+    '''
+    鼠标点击
+    :param clicks: 点击次数
+    :param button: 鼠标键位
+    :param interval: 点击时间间隔
+    :return:
+    '''
+    pyautogui.click(button=button, clicks=clicks, interval=interval)
+
+def check_position_in_screen(x,y):
+    '''
+    检查坐标是否在屏幕内
+    :param x: 横坐标
+    :param y: 纵坐标
+    :return: True/False
+    '''
+    return pyautogui.onScreen(0, 0)
 
 def scroll(amount_to_scroll):
     '''
@@ -62,7 +92,8 @@ def main():
         # print(get_img_position("wechat.png"))
         # shot_screen()
         # move_mouse_rel(-100,100)
-        scroll(1000)
+        # scroll(1000)
+        mouse_click(1,'right')
     except Exception as e:
         print(e)
 
